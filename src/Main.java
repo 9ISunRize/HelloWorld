67ca0bdb4f5d1c99ae0d8ede4da5a9c773dcfc2f
Диана <dianamingazova162@gmail.com>
2022-11-30 00:19:28 +0300

    Начальный коммит

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! Tanci s bubnami");
+        System.out.println("Hello world! Tancci s bubnami");
     }
 }
