8ff07c7e09fe660e1563e9dcd3b94a7bdd86bb9e
Диана <dianamingazova162@gmail.com>
2022-11-30 00:27:09 +0300

    Поменяли вывод

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! Tancci s bubnami");
+        System.out.println("Привет, мир!");
     }
 }
